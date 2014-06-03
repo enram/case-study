fc460979891ca5f193e254cd17874e8fefd47751
Peter Desmet <peter.desmet.work@gmail.com>
2014-06-04 00:19:19 +0200

    New SQL

--- data/wind/query_ecmwfwind_enramstsm.sql
+++ data/wind/query_ecmwfwind_enramstsm.sql
@@ -3,7 +3,7 @@
 
 select a.modelts, a.latitude/100 as latitude, a.longitude/100 as longitude, a.u10, a.v10, a.u_1000, a.v_1000, a.u_925, a.v_925, a.u_850, a.v_850, a.u_700, a.v_700
 from ecmwf.analysis as a
-where (latitude/100 between 49 and 54) and (longitude/100 between 3 and 7.5)
+where (latitude/100 between 49 and 54) and (longitude/100 between 2.5 and 7.5)
 AND (modelts between '2013-04-05 00:00:00' and '2013-05-13 00:00:00')
 ORDER By modelts, latitude,longitude
 
@@ -13,6 +13,6 @@ ORDER By modelts, latitude,longitude
 select f.modelts, f.latitude/100 as latitude, f.longitude/100 as longitude, f.u10, f.v10, f.u_1000, f.v_1000, f.u_925, f.v_925, 
 f.u_850, f.v_850, f.u_700, f.v_700
 from ecmwf.forecast as f
-where (latitude/100 between 49 and 54) and (longitude/100 between 3 and 7.5)
+where (latitude/100 between 49 and 54) and (longitude/100 between 2.5 and 7.5)
 AND (modelts between '2013-04-05 00:00:00' and '2013-05-13 00:00:00')
 ORDER By modelts, latitude,longitude
